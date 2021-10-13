d7005cb1808489e7c43103b42a22baa0646e2313
Bryan J. Brown <bjbrown@fsu.edu>
2021-10-13 16:27:40 -0500

    Updates build script to include logging

--- build/build.sh
+++ build/build.sh
@@ -1,24 +1,24 @@
-rm -f /etc/localtime
-ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime
+rm -f /etc/localtime >> /root/build.log 2>&1
+ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime >> /root/build.log 2>&1
 
-fallocate -l 2G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-swapon -s
+fallocate -l 2G /swapfile >> /root/build.log 2>&1
+chmod 600 /swapfile >> /root/build.log 2>&1
+mkswap /swapfile >> /root/build.log 2>&1
+swapon /swapfile >> /root/build.log 2>&1
+swapon -s >> /root/build.log 2>&1
 echo "/swapfile none swap sw 0 0" >> /etc/fstab
 echo "vm.swappiness=10" >> /etc/sysctl.conf
-sysctl -p
+sysctl -p >> /root/build.log 2>&1
 
-apt update -y
-apt install -y php
-apt install -y php-gd
-apt install -y php-xml
-apt install -y php-curl
-apt install -y php-mbstring
-apt install -y composer
-apt install -y mysql-client
-apt install -y apache2
+apt update -y >> /root/build.log 2>&1
+apt install -y php >> /root/build.log 2>&1
+apt install -y php-gd >> /root/build.log 2>&1
+apt install -y php-xml >> /root/build.log 2>&1
+apt install -y php-curl >> /root/build.log 2>&1
+apt install -y php-mbstring >> /root/build.log 2>&1
+apt install -y composer >> /root/build.log 2>&1
+apt install -y mysql-client >> /root/build.log 2>&1
+apt install -y apache2 >> /root/build.log 2>&1
 
 echo "AddHandler php5-script .php" >> /etc/apache2/apache2.conf
 echo "AddType text/html .php" >> /etc/apache2/apache2.conf
@@ -28,11 +28,12 @@ cd /var/www/html
 rm index.html
 export HOME='/root'
 
-composer -n global require zaporylie/composer-drupal-optimizations
-composer -n create-project drupal-composer/drupal-project:9.x-dev drupal --stability dev --no-interaction
+composer -n global require zaporylie/composer-drupal-optimizations >> /root/build.log 2>&1
+composer -n create-project drupal-composer/drupal-project:9.x-dev drupal --stability dev --no-interaction >> /root/build.log 2>&1
 
 cd /var/www/html/drupal
 rm composer.*
 cp /submit_diginole_config/assets/composer.json .
+composer -n install >> /root/build.log 2>&1
 
-service apache2 restart
+service apache2 restart >> /root/build.log 2>&1
