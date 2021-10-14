f4784e9be2ab3e0f1e1580140e55249aae509b94
Bryan J. Brown <bjbrown@fsu.edu>
2021-10-13 23:09:54 -0500

    WIP MySQL config

--- build/build.sh
+++ build/build.sh
@@ -2,9 +2,13 @@
 
 source /etc/environment
 
+
+# Set timezone
 rm -f /etc/localtime >> /root/build.log 2>&1
 ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime >> /root/build.log 2>&1
 
+
+# Set up swap space so Composer doesn't die on small instances 
 fallocate -l 2G /swapfile >> /root/build.log 2>&1
 chmod 600 /swapfile >> /root/build.log 2>&1
 mkswap /swapfile >> /root/build.log 2>&1
@@ -14,6 +18,8 @@ echo "/swapfile none swap sw 0 0" >> /etc/fstab
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 sysctl -p >> /root/build.log 2>&1
 
+
+# Install os packages
 apt update -y >> /root/build.log 2>&1
 apt install -y php >> /root/build.log 2>&1
 apt install -y php-gd >> /root/build.log 2>&1
@@ -25,20 +31,71 @@ apt install -y mysql-client >> /root/build.log 2>&1
 apt install -y composer >> /root/build.log 2>&1
 apt install -y apache2 >> /root/build.log 2>&1
 
+
+# Set up Drupal user in database
+#mysql \
+#  --user="${DATABASE_ROOT_USERNAME}" \
+#	--password="${DATABASE_ROOT_PASSWORD}" \
+#	--host="${DATABASE_ENDPOINT}" \
+#	--execute="CREATE DATABASE submit_diginole;"
+#
+#mysql \
+#  --user="${DATABASE_ROOT_USERNAME}" \
+#	--password="${DATABASE_ROOT_PASSWORD}" \
+#	--host="${DATABASE_ENDPOINT}" \
+#	--execute="CREATE USER '${DATABASE_DRUPAL_USERNAME}'@'%' IDENTIFIED BY '${DATABASE_DRUPAL_PASSWORD}';"
+#
+#mysql \
+#  --user="${DATABASE_ROOT_USERNAME}" \
+#	--password="${DATABASE_ROOT_PASSWORD}" \
+#	--host="${DATABASE_ENDPOINT}" \
+#	--execute="GRANT ALL PRIVILEGES ON submit_diginole.* TO '${DATABASE_DRUPAL_USERNAME}'@'%';"
+#
+#mysql \
+#  --user="${DATABASE_ROOT_USERNAME}" \
+#	--password="${DATABASE_ROOT_PASSWORD}" \
+#	--host="${DATABASE_ENDPOINT}" \
+#	--execute="FLUSH PRIVILEGES;"
+#
+#service mysql restart
+
+
+# Install Drupal site
+#cd /var/www/html/drupal
+#/var/www/html/drupal/vendor/bin/drupal site:install standard \
+#	--langcode="en" \
+#	--db-type="mysql" \
+#	--db-host="${DATABASE_ENDPOINT}" \
+#	--db-name="submit_diginole" \
+#	--db-user="${DATABASE_DRUPAL_USERNAME}" \
+#	--db-pass="${DATABASE_DRUPAL_PASSWORD}" \
+#	--db-port="3306" \
+#	--site-mail="${DRUPAL_ADMIN_EMAIL}" \
+#	--account-name="${DRUPAL_ADMIN_USERNAME}" \
+#	--account-pass="${DRUPAL_ADMIN_PASSWORD}" \
+#	--account-mail="${DRUPAL_ADMIN_EMAIL}" \
+#	--no-interaction
+#/submit_diginole_config/commands/sd-import
+
+
+# Prep Apache for Drupal
 echo "AddHandler php5-script .php" >> /etc/apache2/apache2.conf
 echo "AddType text/html .php" >> /etc/apache2/apache2.conf
 sed -i -e 's/AllowOverride\ None/AllowOverride\ All/g' /etc/apache2/apache2.conf
 sed -i -e 's/\/var\/www\/html/\/var\/www\/html\/drupal\/web/g' /etc/apache2/sites-available/000-default.conf
 cd /var/www/html
 rm index.html
-export HOME='/root'
 
+
+# Install Drupal
+export HOME='/root'
 composer -n global require zaporylie/composer-drupal-optimizations >> /root/build.log 2>&1
 composer -n create-project drupal-composer/drupal-project:9.x-dev drupal --stability dev --no-interaction >> /root/build.log 2>&1
-
 cd /var/www/html/drupal
 rm composer.*
 cp /submit_diginole_config/assets/composer.json .
 composer -n install >> /root/build.log 2>&1
 
+
+# Preflight
 service apache2 restart >> /root/build.log 2>&1
--- build/vagrant.sh
+++ build/vagrant.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Set local config
 cat /submit_diginole_config/assets/bash.sh >> /root/.bashrc
-
 cat /submit_diginole_config/assets/vagrant.env >> /etc/environment
 
+# Install local MySQL
+#apt -y install mysql-server
+#service mysql start
+
+# Initiate build
 /submit_diginole_config/build/build.sh
