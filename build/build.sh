b38978115b93507c858ec4a829f8546419ea8752
Bryan J. Brown <bjbrown@fsu.edu>
2021-11-10 17:16:41 -0600

    Refactors build scripts to prep for Drupal9 AMI

--- build/build.sh
+++ build/build.sh
@@ -20,19 +20,6 @@ echo "vm.swappiness=10" >> /etc/sysctl.conf
 sysctl -p >> /root/build.log 2>&1
 
 
-# Install os packages
-apt update -y >> /root/build.log 2>&1
-apt install -y php >> /root/build.log 2>&1
-apt install -y php-gd >> /root/build.log 2>&1
-apt install -y php-xml >> /root/build.log 2>&1
-apt install -y php-curl >> /root/build.log 2>&1
-apt install -y php-mbstring >> /root/build.log 2>&1
-apt install -y php-mysql >> /root/build.log 2>&1
-apt install -y mysql-client >> /root/build.log 2>&1
-apt install -y composer >> /root/build.log 2>&1
-apt install -y apache2 >> /root/build.log 2>&1
-
-
 # Set up Drupal user in database
 mysql \
   --user="${DATABASE_ROOT_USERNAME}" \
--- build/vagrant.sh
+++ build/vagrant.sh
@@ -4,6 +4,18 @@
 cat /submit_diginole_config/assets/bash.sh >> /root/.bashrc
 cat /submit_diginole_config/assets/vagrant.env >> /etc/environment
 
+# Install os packages
+apt update -y >> /root/build.log 2>&1
+apt install -y php >> /root/build.log 2>&1
+apt install -y php-gd >> /root/build.log 2>&1
+apt install -y php-xml >> /root/build.log 2>&1
+apt install -y php-curl >> /root/build.log 2>&1
+apt install -y php-mbstring >> /root/build.log 2>&1
+apt install -y php-mysql >> /root/build.log 2>&1
+apt install -y mysql-client >> /root/build.log 2>&1
+apt install -y composer >> /root/build.log 2>&1
+apt install -y apache2 >> /root/build.log 2>&1
+
 # Install local MySQL
 apt update -y >> /root/vagrant.log 2>&1
 apt -y install mysql-server >> /root/vagrant.log 2>&1
