4f42752cece4a5a585bbfd38f673c8764765cd81
Bryan J. Brown <bjbrown@fsu.edu>
2022-03-14 00:14:52 -0400

    Removes post-install.sh script installing submit_diginole_content

--- build/post-install.sh
+++
@@ -1,4 +0,0 @@
-# Copy in submit_diginole_content module
-mkdir /multisites/submit_diginole/web/modules/custom
-cd /multisites/submit_diginole/web/modules/custom
-cp -r  /submit_diginole_config/assets/submit_diginole_content .
